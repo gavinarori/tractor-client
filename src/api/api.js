0788efe12796f2ce5661c55a57cca2d4726d97db
gavinarori <arorigavin@gmail.com>
2024-11-20 21:06:05 +0300

    changed the base url to match the deployed server

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const local = 'http://localhost:5000'
+const local = 'https://tractor-backend-xk1t.onrender.com'
 const production = ''
 const api = axios.create({
     baseURL: `${local}/api`,
